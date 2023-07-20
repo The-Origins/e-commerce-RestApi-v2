a8f74a60189952fc45d6a78e5a34252116cb15fa
The-Origins <69356829+The-Origins@users.noreply.github.com>
2023-07-20 14:46:01 +0300

    added status code that will be sent to the error handler

--- controllers/user/deleteUser.js
+++ controllers/user/deleteUser.js
@@ -6,6 +6,7 @@ const deleteUser = (req,res,next) =>
     {
         if(!user)
         {
+            res.code = 400
             return next(new Error(`No user with id: ${req.params.id}`))
         }
         res.json({success:true, data:user, message:`Deleted user '${user.name.first}' from users`})
