00416e2b22b565b8b92bef2c9299950c0c569445
The-Origins <69356829+The-Origins@users.noreply.github.com>
2023-07-20 14:35:54 +0300

    added status code logic for the errorhandler

--- controllers/middleware/errorHandler.js
+++ controllers/middleware/errorHandler.js
@@ -1,8 +1,9 @@
 const errorHandler = (err, req, res, next) =>
 {
+    res.code = res.code ? res.code : 500
     if(err)
     {
-        res.json({success:false, data:[], message:err.message})
+        res.status(res.code).json({success:false, data:[], message:err.message})
     }
     next()
 }
