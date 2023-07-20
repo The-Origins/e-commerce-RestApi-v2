8d782073873e31cee9ea2f3e9af2be7c83500a8a
The-Origins <69356829+The-Origins@users.noreply.github.com>
2023-07-20 14:42:05 +0300

    added status code that will be sent to the error handler

--- controllers/product/deleteProduct.js
+++ controllers/product/deleteProduct.js
@@ -6,6 +6,7 @@ const deleteProduct = (req,res,next) =>
     {
         if(!product)
         {
+            res.code = 400
             return next(new Error(`No product with id: ${req.params.id}`))
         }
         res.json({success:true, data:product, message:`Deleted product '${product.name}' from products`})
